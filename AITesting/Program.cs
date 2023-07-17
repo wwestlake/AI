3ae29fdd19adc6443916a62e3a88ad96ad00d5db
William Westlake <wwestlake@lagdaemon.com>
2023-07-17 15:49:28 -0400

    updated

--- AITesting/Program.cs
+++ AITesting/Program.cs
@@ -8,17 +8,15 @@
             bb.Set<int>("Speed", 10);
 
             var child = bb.Create("Child");
-            var x = bb.Create("Child");
-            x.Process(
-            //child.Process(
-            (x) => {
-                x.Set<float>("Score", 100.0f);
+            child.Process(
+                (x) => {
+                    x.Set<float>("Score", 100.0f);
 
-                var speed = x.Get<int>("Speed");
-                var score = x.Get<float>("Score");
+                    var speed = x.Get<int>("Speed");
+                    var score = x.Get<float>("Score");
 
-                Console.WriteLine($"{speed} {bb.GetType("Speed")} {score} {x.GetType("Score")} ");
-            });
+                    Console.WriteLine($"{speed} {bb.GetType("Speed")} {score} {x.GetType("Score")} ");
+                });
 
         }
     }
