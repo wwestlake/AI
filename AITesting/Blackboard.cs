d1edb6976d9781378e8df32d5c1658f4aea2030f
William Westlake <wwestlake@lagdaemon.com>
2023-07-17 13:56:14 -0400

    Blackboard completed

---
+++ AITesting/Blackboard.cs
@@ -0,0 +1,67 @@
+﻿using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITesting
+{
+    public class Blackboard
+    {
+        private readonly object _lockObject = new object();
+
+        private Dictionary<string, dynamic> _map = new Dictionary<string, dynamic>();
+
+        private Blackboard _parent = null;
+        private Dictionary<string, Blackboard> _children = new Dictionary<string, Blackboard>();
+
+        public Blackboard(Blackboard parent = null)
+        {
+            _parent = parent;
+        }
+
+        public  Result<Blackboard> Create(string name)
+        {
+            lock (_lockObject)
+            {
+                if (_children.ContainsKey(name)) { Result.Fail($"Blackboard {name} already exists.");  }
+                var bb = new Blackboard(this);
+                _children[name] = bb;
+                return Result.Ok(bb);
+            }
+        }
+
+        public Result<T> Get<T>(string name)
+        {
+            lock (_lockObject)
+            {
+                if (! _map.ContainsKey(name)) 
+                {
+                    if (_parent == null) return Result.Fail($"Key {name} not found");
+                    return _parent.Get<T>(name); 
+                }
+                var result = _map[name];
+                if (result is T) return Result.Ok((T)result);
+                return Result.Fail($"Value of {name} is not type {typeof(T).Name}");
+            }
+        }
+
+        public Result<T> Set<T>(string name, T value)
+        {
+            lock (_lockObject)
+            {
+                if (! _map.ContainsKey(name)) 
+                { 
+                    _map.Add(name, value);
+                    return Result.Ok(value);
+                } else
+                {
+                    _map[name] = value;
+                    return Result.Ok(value);
+                }
+            }
+        }
+
+    }
+}
---
+++ AITesting/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AITesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bb = new Blackboard();
+            bb.Set<int>("Speed", 10);
+
+            var child = bb.Create("Child");
+            if (child.IsSuccess)
+            {
+                Blackboard? cbb = child.Value;
+                cbb.Set<float>("Score", 100.0f);
+
+                var speed = cbb.Get<int>("Speed");
+                var score = cbb.Get<float>("Score");
+
+                Console.WriteLine($"{speed} {score}");
+            }
+
+        }
+    }
+}
